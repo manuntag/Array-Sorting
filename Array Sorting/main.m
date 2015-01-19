63a33eb0944f477f67335d276db96bc9c3795a0e
manuntag <manuntag@ualberta.ca>
2015-01-19 15:42:17 -0800

    Initial Commit

---
+++ Array Sorting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Array Sorting
+//
+//  Created by David Manuntag on 2015-01-19.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
